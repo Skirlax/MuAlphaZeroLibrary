211764ddd9dc630ea114e7bfbb1a2e889fb4a2c7
skyr <skirlaxxxx@gmail.com>
2024-02-25 18:59:23 +0100

    Initial release version.

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     version="1.0",
     description="Library for running and training MuZero and AlphaZero models.",
     author="Skyr",
-    install_requires=get_requirements(),
     package_data={
         "mu_alpha_zero": ["*.txt", "*.root"]
     }
