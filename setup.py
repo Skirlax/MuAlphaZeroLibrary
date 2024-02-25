c26f2dd6175a41402fa353496b724afceff66e44
skyr <skirlaxxxx@gmail.com>
2024-02-25 19:58:49 +0100

    Initial release version.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     version="1.0.1",
     description="Library for running and training MuZero and AlphaZero models.",
     author="Skyr",
+    install_requires=open("requirements.txt").read().strip().split("\n"),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     package_data={
