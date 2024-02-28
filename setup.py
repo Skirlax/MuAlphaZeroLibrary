d092cc0088358cc22c29b04496506e6c9e2b70fd
skyr <skirlaxxxx@gmail.com>
2024-02-28 23:11:11 +0100

    Added url parameter to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,8 @@
 from setuptools import setup
-from setuptools.command.install import install
-import subprocess
-
-
-def get_requirements():
-    with open("requirements.txt", "r") as file:
-        return file.read().strip().split("\n")
-
-
-
-
 
 setup(
     name="mu_alpha_zero_library",
-    version="1.0.4",
+    version="1.0.4.1",
     description="Library for running and training MuZero and AlphaZero models.",
     author="Skyr",
     install_requires=open("requirements.txt").read().strip().split("\n"),
@@ -21,6 +10,7 @@ setup(
     long_description_content_type='text/markdown',
     package_data={
         "mu_alpha_zero": ["*.txt", "*.root"]
-    }
+    },
+    url="https://github.com/Skirlax/MuAlphaZeroLibrary",
 
 )
