c31a1c55f94a55ed2d2e6efe010e04db0f356774
Skyr <skirlaxxx@gmail.com>
2024-03-17 00:37:09 +0100

    Testing c++ support.

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ from setuptools import setup
 from torch.utils import cpp_extension
 
 setup(name="mu_alpha_zero_library", version="1.0.5",
-    description="Library for running and training MuZero and AlphaZero models.", author="Skyr", ext_modules=[
-        cpp_extension.CppExtension('CppSelfPlay', ['CppSelfPlay/MCTS/Node.cpp',
-                                                     'CppSelfPlay/MCTS/MuzeroSearchTree.cpp',
-                                                     'CppSelfPlay/Buffers/MuZeroFrameBuffer.cpp',
-                                                     'CppSelfPlay/utils.cpp',
-                                                     'CppSelfPlay/Network/MuZeroDefaultNet.cpp',
-                                                     'CppSelfPlay/CppSelfPlay.cpp'],
+      description="Library for running and training MuZero and AlphaZero models.", author="Skyr", ext_modules=[
+        cpp_extension.CppExtension('CppSelfPlay',
+                                   ['CppSelfPlay/Buffers/MuZeroFrameBuffer.cpp', 'CppSelfPlay/MCTS/Node.cpp',
+                                    'CppSelfPlay/MCTS/MuzeroSearchTree.cpp', 'CppSelfPlay/utils.cpp',
+                                    'CppSelfPlay/Network/MuZeroDefaultNet.cpp', 'CppSelfPlay/CppSelfPlay.cpp'],
                                    include_dirs=['CppSelfPlay/', 'CppSelfPlay/MCTS/', 'CppSelfPlay/Buffers/',
                                                  'CppSelfPlay/Network/', 'CppSelfPlay/Network/'])],
-    cmdclass={'build_ext': cpp_extension.BuildExtension},
-    install_requires=open("requirements.txt").read().strip().split("\n"),
-    long_description=open('README.md', encoding="utf-8").read(), long_description_content_type='text/markdown',
-    package_data={"mu_alpha_zero": ["*.txt", "*.root"]}, url="https://github.com/Skirlax/MuAlphaZeroLibrary",
+      cmdclass={'build_ext': cpp_extension.BuildExtension},
+      install_requires=open("requirements.txt").read().strip().split("\n"),
+      long_description=open('README.md', encoding="utf-8").read(), long_description_content_type='text/markdown',
+      package_data={"mu_alpha_zero": ["*.txt", "*.root"]}, url="https://github.com/Skirlax/MuAlphaZeroLibrary",
 
-)
+      )
