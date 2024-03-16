7a49d48c68024141fee4ea5540ab3062642f0205
Skyr <skirlaxxx@gmail.com>
2024-03-16 23:45:17 +0100

    Testing c++ support.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from torch.utils import cpp_extension
 
 setup(name="mu_alpha_zero_library", version="1.0.5",
     description="Library for running and training MuZero and AlphaZero models.", author="Skyr", ext_modules=[
-        cpp_extension.CppExtension('CppSelfPlayDir', ['CppSelfPlayDir/MCTS/Node.cpp',
+        cpp_extension.CppExtension('CppSelfPlay', ['CppSelfPlayDir/MCTS/Node.cpp',
                                                      'CppSelfPlayDir/MCTS/MuzeroSearchTree.cpp',
                                                      'CppSelfPlayDir/Buffers/MuZeroFrameBuffer.cpp',
                                                      'CppSelfPlayDir/utils.cpp',
