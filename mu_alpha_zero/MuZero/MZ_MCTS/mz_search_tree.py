4369f07449b3a3c6dcc913ffe433bdafc038fb35
Skyr <skirlaxxx@gmail.com>
2024-03-24 15:04:43 +0100

    Testing shared memory between processes.

--- mu_alpha_zero/MuZero/MZ_MCTS/mz_search_tree.py
+++ mu_alpha_zero/MuZero/MZ_MCTS/mz_search_tree.py
@@ -13,6 +13,7 @@ from mu_alpha_zero.MuZero.Network.networks import MuZeroNet
 from mu_alpha_zero.MuZero.utils import match_action_with_obs, resize_obs, scale_action, scale_reward, scale_state
 from mu_alpha_zero.config import MuZeroConfig
 from mu_alpha_zero.mem_buffer import MuZeroFrameBuffer
+from multiprocessing.managers import BaseManager
 
 
 class MuZeroSearchTree(SearchTree):
@@ -124,6 +125,10 @@ class MuZeroSearchTree(SearchTree):
     @staticmethod
     def parallel_self_play(nets: list, trees: list, memory: GeneralMemoryBuffer, device: th.device, num_games: int,
                            num_jobs: int):
+        BaseManager.register("MemBuffer", callable=lambda: memory)
+        manager = BaseManager()
+        manager.start()
+        mem = manager.MemBuffer()
         with Pool(num_jobs) as p:
             if memory.is_disk and memory.full_disk:
                 results = p.starmap(p_self_play, [
@@ -131,8 +136,8 @@ class MuZeroSearchTree(SearchTree):
                     range(len(nets))])
             else:
                 results = p.starmap(p_self_play,
-                                    [(nets[i], trees[i], copy.deepcopy(device), num_games // num_jobs, None) for i in
-                                        range(len(nets))])
+                                    [(nets[i], trees[i], copy.deepcopy(device), num_games // num_jobs, mem) for i in
+                                     range(len(nets))])
         for result in results:
             memory.add_list(result)
 
