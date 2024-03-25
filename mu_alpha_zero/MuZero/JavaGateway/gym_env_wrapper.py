c03d4ac7e9c41bbf5d0d502eb369aae64397dff5
Skyr <skirlaxxx@gmail.com>
2024-03-25 13:00:12 +0100

    Added support for saving saves to disk immediately, so that none are stored in memory.

---
+++ mu_alpha_zero/MuZero/JavaGateway/gym_env_wrapper.py
@@ -0,0 +1,45 @@
+import base64
+
+import numpy as np
+from fastapi import APIRouter
+
+from mu_alpha_zero.General.mz_game import MuZeroGame
+
+
+class GymEnvWrapper:
+    def __init__(self, env: MuZeroGame):
+        self.env_instance = env
+        self.router = APIRouter()
+        self.register_routes()
+
+    def register_routes(self):
+        self.router.add_api_route("/frame_skip_step/{action}/{player}/{frame_skip}", self.frame_skip_step,
+                                  methods=["GET"])
+        self.router.add_api_route("/reset", self.reset, methods=["GET"])
+        self.router.add_api_route("/get_noop", self.get_noop, methods=["GET"])
+        self.router.add_api_route("/get_num_actions", self.get_num_actions, methods=["GET"])
+        self.router.add_api_route("/game_result/{player}", self.game_result, methods=["GET"])
+
+    def frame_skip_step(self, action: int, player: int, frame_skip: int):
+        if player == 0:
+            player = None
+        state, reward, done = self.env_instance.frame_skip_step(action, player, frame_skip)
+        return {"state": self.encode_array(state), "reward": reward, "done": done}
+
+    def reset(self):
+        state = self.env_instance.reset()
+        return {"state": self.encode_array(state)}
+
+    def get_noop(self):
+        return self.env_instance.get_noop()
+
+    def get_num_actions(self):
+        return self.env_instance.get_num_actions()
+
+    def game_result(self, player: int):
+        if player == 0:
+            player = None
+        return self.env_instance.game_result(player)
+
+    def encode_array(self, array: np.ndarray):
+        return base64.b64encode(array.tobytes()).decode("utf-8")
