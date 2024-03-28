ad38c324f0253bda44025f4adb0972d1d525289a
Skyr <skirlaxxx@gmail.com>
2024-03-28 16:12:51 +0100

    Added hook support amongst others.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mu_alpha_zero_library",
-    version="1.0.9.1",
+    version="1.0.9.2",
     description="Library for running and training MuZero and AlphaZero models.",
     author="Skyr",
     install_requires=open("requirements.txt").read().strip().split("\n"),
