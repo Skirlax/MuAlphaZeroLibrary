9153f4089d32de037583e892e3e916c342c65014
Skyr <skirlaxxx@gmail.com>
2024-03-30 20:11:29 +0100

    Saving arrays using gzip.

--- mu_alpha_zero/MuZero/lazy_arrays.py
+++ mu_alpha_zero/MuZero/lazy_arrays.py
@@ -1,18 +1,27 @@
-import uuid
-import atexit
-import numpy as np
+import gzip
 import os
+import uuid
+
+import numpy as np
 
 
 class LazyArray:
     def __init__(self, array: np.ndarray, directory_path: str):
         self.directory_path = directory_path
-        self.path = f"{directory_path}/array_{uuid.uuid4()}.npy"
+        self.path = f"{directory_path}/array_{uuid.uuid4()}.npy.gz"
         self.persistent = False
-        np.save(self.path, array)
+        self.save_array(array)
 
     def load_array(self):
-        return np.load(self.path)
+        f = gzip.GzipFile(self.path, 'r')
+        arr = np.load(f)
+        f.close()
+        return arr
+
+    def save_array(self, array: np.ndarray):
+        file = gzip.GzipFile(self.path, 'w')
+        np.save(file, array)
+        file.close()
 
     def __array__(self):
         return self.load_array()
