13d7da17db24c9da4ecbf147bdd78adbeb6fed2b
skyr <skirlaxxx@gmail.com>
2024-04-04 14:31:04 +0200

    Improvements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mu_alpha_zero_library",
-    version="1.0.9.2",
+    version="1.0.9.3",
     description="Library for running and training MuZero and AlphaZero models.",
     author="Skyr",
     install_requires=open("requirements.txt").read().strip().split("\n"),
