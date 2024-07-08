40f352d120c86190293c21d1d8f169c8b91c4e9a
skyr <skirlaxxx@gmail.com>
2024-07-08 10:51:17 +0200

    Testing new parallel training.

--- mu_alpha_zero/shared_storage_manager.py
+++ mu_alpha_zero/shared_storage_manager.py
@@ -11,16 +11,16 @@ class SharedStorage:
         self.stable_network_params: dict = None
 
     def get_experimental_network_params(self):
-        return self.experimental_network_params
+        return self.experimental_network_params.clone()
 
     def set_experimental_network_params(self, network_params: dict):
-        self.experimental_network_params = network_params
+        self.experimental_network_params = network_params.clone()
 
     def get_stable_network_params(self):
-        return self.stable_network_params
+        return self.stable_network_params.clone()
 
     def set_stable_network_params(self, network_params: dict):
-        self.stable_network_params = network_params
+        self.stable_network_params = network_params.clone()
 
     def get_mem_buffer(self):
         return self.mem_buffer
