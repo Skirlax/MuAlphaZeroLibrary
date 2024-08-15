70ac9bc63257c90f2b13e0637f7123a5074edb7f
skyr <skirlaxxx@gmail.com>
2024-08-15 12:16:55 +0200

    Added parallel training to alpha zero.

--- mu_alpha_zero/AlphaZero/Network/nnet.py
+++ mu_alpha_zero/AlphaZero/Network/nnet.py
@@ -297,7 +297,7 @@ class OriginalAlphaZeroNetwork(nn.Module, GeneralAlphZeroNetwork):
         device = th.device("cuda" if th.cuda.is_available() else "cpu")
         states, pi, v, _, masks = experience_batch[0], experience_batch[1], experience_batch[2], experience_batch[3], \
             experience_batch[4]
-        pi = [[y for y in x.values()] for x in pi if isinstance(x,dict)]
+        pi = [[y for y in x.values()] if isinstance(x, dict) else x for x in pi]
         # game = [[y.frame,y.pi,y.v,y.action_mask] for y in experience_batch.datapoints]
         # states, pi, v, masks = zip(*game)
         states = th.tensor(np.array(states), dtype=th.float32, device=device)
