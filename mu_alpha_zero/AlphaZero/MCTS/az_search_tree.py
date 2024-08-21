6f59d8db6e8988b5dc829585efd28033dcd6b7f8
skyr <skirlaxxx@gmail.com>
2024-08-22 00:11:32 +0200

    Added option to use the true game state in the search tree.

--- mu_alpha_zero/AlphaZero/MCTS/az_search_tree.py
+++ mu_alpha_zero/AlphaZero/MCTS/az_search_tree.py
@@ -78,8 +78,13 @@ class McSearchTree(SearchTree):
                     game_history = [(x[0], x[1], r * current_player * x[3], x[3], x[4]) for x in game_history]
 
                 # append the terminal state
-                game_history.append(((state * -current_player), (np.ones((len(pi),), dtype=np.float32) / len(pi)).tolist(), -r,
-                                     -current_player, np.zeros(len(pi))))
+                if -1 < r < 1:
+                    reward = r
+                else:
+                    reward = -r
+                game_history.append(
+                    ((state * -current_player), (np.ones((len(pi),), dtype=np.float32) / len(pi)).tolist(), reward,
+                     -current_player, np.zeros(len(pi))))
                 break
             current_player *= -1
 
