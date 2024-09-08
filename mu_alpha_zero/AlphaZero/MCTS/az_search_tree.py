Fixed noise calculation

mu_alpha_zero/AlphaZero/MCTS/az_search_tree.py
@@ -114,9 +114,9 @@ class McSearchTree(SearchTree):
         state_ = th.tensor(state_, dtype=th.float32, device=device).unsqueeze(0)
         probabilities, v = network.predict(state_, muzero=False)
         if self.alpha_zero_config.add_dirichlet_noise:
-            probabilities = (
-                                    1 - self.alpha_zero_config.dirichlet_alpha) * probabilities + self.alpha_zero_config.dirichlet_alpha * np.random.dirichlet(
-                [0.03] * len(probabilities))
+            d = np.random.dirichlet([self.alpha_zero_config.dirichlet_alpha] * self.alpha_zero_config.net_action_size)
+            probabilities = (1 - self.alpha_zero_config.epsilon) * probabilities + self.alpha_zero_config.epsilon * d
+            probabilities = probabilities.cpu().detach().numpy()
         probabilities = mask_invalid_actions(probabilities,
                                              self.game_manager.get_invalid_actions(state.copy(), current_player))
         probabilities = probabilities.flatten().tolist()

mu_alpha_zero/MuZero/MZ_MCTS/mz_search_tree.py
@@ -125,7 +125,10 @@ class MuZeroSearchTree(SearchTree):
             state_ = scale_hidden_state(state_)
         pi, v = network_wrapper.prediction_forward(state_.unsqueeze(0), predict=True)
         if self.muzero_config.dirichlet_alpha > 0:
-            pi = pi + np.random.dirichlet([self.muzero_config.dirichlet_alpha] * self.muzero_config.net_action_size)
+            d = np.random.dirichlet([self.muzero_config.dirichlet_alpha] * self.muzero_config.net_action_size)
+            pi = (1 - self.muzero_config.epsilon) * pi + self.muzero_config.epsilon * th.tensor(d, device=device,
+                                                                                                dtype=th.float32)
+            pi = pi.cpu().detach().numpy()
         pi = mask_invalid_actions(self.game_manager.get_invalid_actions(current_player), pi)
         pi = pi.flatten().tolist()
         root_node.expand_node(state_, pi, 0)