316043626dfc4ed5336753480a01594da4256e84
skyr <skirlaxxx@gmail.com>
2024-09-28 14:27:08 +0200

    Fixes

--- mu_alpha_zero/MuZero/MZ_MCTS/mz_search_tree.py
+++ mu_alpha_zero/MuZero/MZ_MCTS/mz_search_tree.py
@@ -125,7 +125,6 @@ class MuZeroSearchTree(SearchTree):
         if self.muzero_config.dirichlet_alpha > 0:
             d = np.random.dirichlet([self.muzero_config.dirichlet_alpha] * self.muzero_config.net_action_size)
             pi = (1 - self.muzero_config.epsilon) * pi + self.muzero_config.epsilon * d
-            pi = pi.cpu().detach().numpy()
         pi = mask_invalid_actions(self.game_manager.get_invalid_actions(current_player), pi)
         pi = pi.flatten().tolist()
         root_node.expand_node(state_, pi, 0)
